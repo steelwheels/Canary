aa7dfa663aff42d0ae82c308dcba6b2e541bd6ae
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-01-17 09:44:44 +0900

    test: Add UnitTest for unit test on console

---
+++ UnitTest/UnitTest/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  UnitTest
+//
+//  Created by Tomoo Hamada on 2016/01/17.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!. Good morning")
+
+let a : UInt = 123
+let num = a as NSNumber
+let val = num.unsignedIntegerValue
+print("a = \(val)")
