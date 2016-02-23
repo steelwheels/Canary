5c98758271c53bba5d7106a776443b3f6b9ca31f
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-02-24 01:14:43 +0900

    CNState: Add unit test for CNState, CNCombinedState and CNStateObserver class

---
+++ UnitTest/UTState/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UTState
+//
+//  Created by Tomoo Hamada on 2016/02/24.
+//  Copyright © 2016年 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+		UTStateTest()
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
