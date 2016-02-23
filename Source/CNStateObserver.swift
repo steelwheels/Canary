9ecb17dab8b759b58a5a6082ad6af69518dc61de
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-02-24 00:41:52 +0900

    CNStateObserver: Initial commit of this class

---
+++ Source/CNStateObserver.swift
@@ -0,0 +1,50 @@
+/*
+ * @file	CNStateObserver.h
+ * @brief	Define CNStateObserver class
+ * @par Copyright
+ *   Copyright (C) 2016 Steel Wheels Project
+ */
+
+import Foundation
+
+public class CNStateObserver : NSObject
+{
+	private dynamic var mState : CNState? = nil
+	private var mCallback : ((state : CNState) -> Void)? = nil
+	
+	deinit {
+		if let curstate = mState {
+			curstate.removeStateObserver(self)
+		}
+	}
+	
+	public var state : CNState? {
+		get	{
+			return mState
+		}
+		set(newstate){
+			if let curstate = mState {
+				curstate.removeStateObserver(self)
+			}
+			if let nextstate = newstate {
+				nextstate.addStateObserver(self)
+			}
+			mState = newstate
+		}
+	}
+	
+	public var callback : ((state : CNState) -> Void)? {
+		get { return mCallback }
+		set(newcallback) { mCallback = newcallback }
+	}
+
+	public override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+		if let state = object as? CNState {
+			if keyPath == CNState.stateKey {
+				if let callback = mCallback {
+					callback(state: state)
+				}
+			}
+		}
+	}
+}
