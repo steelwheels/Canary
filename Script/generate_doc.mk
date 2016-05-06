6f29a928dc8f7abc69066799bf38fceb38d01103
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-05-06 17:08:21 +0900

    doc: Update document generation script

--- Script/generate_doc.mk
+++ Script/generate_doc.mk
@@ -8,7 +8,7 @@ PROJECT_DIR	= ../OSX
 DOCUMENT_DIR	= ../Document/html
 README_FILE	= ../README.md
 
-all: dummy
+all: document_dir
 	(cd $(PROJECT_DIR) ; \
 	 jazzy -o $(DOCUMENT_DIR) \
 	   --author "Steel Wheels Project" \
@@ -17,6 +17,11 @@ all: dummy
 	   --module $(MODULE_NAME) \
 	   --github_url https://github.com/steelwheels/Canary.git \
 	)
+
+document_dir: dummy
+	if [ ! -d ../Document ] ; then \
+		mkdir ../Document ; \
+	fi
 
 dummy:
 
