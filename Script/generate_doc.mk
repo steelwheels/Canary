8155b835cd6c4fd9492b242ad9e04f23ed3f9069
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-05-07 11:59:51 +0900

    doc: Update Makefile

--- Script/generate_doc.mk
+++ Script/generate_doc.mk
@@ -5,23 +5,20 @@
 MODULE_NAME	= Canary
 
 PROJECT_DIR	= ../OSX
-DOCUMENT_DIR	= ../Document/html
+DOCUMENT_DIR	= ../../Document/$(MODULE_NAME)
 README_FILE	= ../README.md
+GITHUB_NAME	= Canary.git
 
-all: document_dir
+all: dummy
 	(cd $(PROJECT_DIR) ; \
+	 mkdir -p $(DOCUMENT_DIR) ; \
 	 jazzy -o $(DOCUMENT_DIR) \
 	   --author "Steel Wheels Project" \
 	   --author_url "https://sites.google.com/site/steelwheelsproject/" \
 	   --readme $(README_FILE) \
 	   --module $(MODULE_NAME) \
-	   --github_url https://github.com/steelwheels/Canary.git \
+	   --github_url https://github.com/steelwheels/$(GITHUB_NAME) \
 	)
-
-document_dir: dummy
-	if [ ! -d ../Document ] ; then \
-		mkdir ../Document ; \
-	fi
 
 dummy:
 
