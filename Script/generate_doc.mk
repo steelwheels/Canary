b75c90af6c9a111fd28921f5595e1d69fcf435ca
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-07-11 00:24:19 +0900

    build: Update script to generate document

--- Script/generate_doc.mk
+++ Script/generate_doc.mk
@@ -5,14 +5,15 @@
 MODULE_NAME	= Canary
 
 PROJECT_DIR	= ../OSX
-DOCUMENT_DIR	= ../../Document/$(MODULE_NAME)
+DOCUMENT_DIR	= ../../Document
 README_FILE	= ../README.md
 GITHUB_NAME	= Canary.git
 
 all: dummy
+	(cd $(DOCUMENT_DIR) ; rm -r $(MODULE_NAME))
 	(cd $(PROJECT_DIR) ; \
-	 mkdir -p $(DOCUMENT_DIR) ; \
-	 jazzy -o $(DOCUMENT_DIR) \
+	 mkdir -p $(DOCUMENT_DIR)/$(MODULE_NAME) ; \
+	 jazzy -o $(DOCUMENT_DIR)/$(MODULE_NAME) \
 	   --author "Steel Wheels Project" \
 	   --author_url "http://steelwheels.github.io/" \
 	   --readme $(README_FILE) \
