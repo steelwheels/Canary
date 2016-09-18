282e86e1b66fea7607ca702af365b608c3d5262d
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2016-09-19 07:45:10 +0900

    build: Add iOS support

--- Script/install.mk
+++ Script/install.mk
@@ -5,7 +5,7 @@
 PROJECT_NAME=Canary
 
 install: dummy
-	xcodebuild install -target $(PROJECT_NAME) \
+	xcodebuild build install -target $(PROJECT_NAME) \
 	  -project $(PROJECT_NAME).xcodeproj \
 	  -configuration Release DSTROOT=/ ONLY_ACTIVE_ARCH=NO
 
