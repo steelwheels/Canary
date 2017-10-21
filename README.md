521812a1e778b493e93af3af8eb84554c78be04b
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2017-10-21 21:19:38 +0900

    CNIndentConsole: Initial commit of this class

--- README.md
+++ README.md
@@ -5,12 +5,14 @@ The Canary Framework is a package of general purpose data structure and classes.
 This is implemented for macOS and iOS.
 
 ## Copyright
-This software is produced by [Steel Wheels Project](https://sites.google.com/site/steelwheelsproject/) and distributed under
+This software is produced by [Steel Wheels Project](http://steelwheels.github.io) and distributed under
 [GNU LESSER GENERAL PUBLIC LICENSE Version 2.1](https://www.gnu.org/licenses/lgpl-2.1-standalone.html).
 
 ## Target
-* macOS: macOS Sierra (10.12.4) or lator
-* iOS: iOS 10.3 or lator
+* macOS: macOS Sierra (10.12.4) or later
+* iOS: iOS 10.3 or later
+* xCode: Xcode9 or later
 
 ## Links
-* [Steel Wheels Project Web Page ](https://github.com/steelwheels):Producer of this framework.
+* [Blog](http://steel-wheels-project.blogspot.jp/): Blog page of this project
+* [Source code repository](https://github.com/steelwheels): Source code repository on GitHub
--- Source/CNConsole.swift
+++ Source/CNConsole.swift
@@ -9,12 +9,8 @@ import Foundation
 
 open class CNConsole
 {
-	public func print(string src: String){
-		flush(string: src)
-	}
-
-	open func flush(string str: String){
-		Swift.print(str, terminator: "")
+	open func print(string src: String){
+		Swift.print(src, terminator:"")
 	}
 }
 
@@ -24,11 +20,47 @@ public class CNFileConsole : CNConsole
 
 	public init(file f: CNTextFile){
 		mTextFile = f
-		super.init()
 	}
-	/* Do not call this method from the outside */
-	public override func flush(string str: String){
+	
+	public override func print(string str: String){
 		mTextFile.print(string: str)
+	}
+}
+
+public class CNIndentConsole: CNConsole
+{
+	private var mConsole: 		CNConsole
+	private var mIndentValue:  	Int
+	private var mIndentString:	String
+
+	public init(console cons: CNConsole){
+		mConsole      = cons
+		mIndentValue  = 0
+		mIndentString = ""
+	}
+
+	public override func print(string src: String){
+		mConsole.print(string: mIndentString + src)
+	}
+
+	public func incrementIndent(){
+		mIndentValue  += 1
+		mIndentString =  CNIndentConsole.indentString(indent: mIndentValue)
+	}
+
+	public func decrementIndent(){
+		if mIndentValue > 0 {
+			mIndentValue -= 1
+			mIndentString =  CNIndentConsole.indentString(indent: mIndentValue)
+		}
+	}
+
+	private class func indentString(indent idt: Int) -> String {
+		var result: String = ""
+		for _ in 0..<idt {
+			result += " "
+		}
+		return result
 	}
 }
 
@@ -39,8 +71,7 @@ public class CNConnectedConsole: CNConsole
 	public init(outputPort port: CNOutputPort<String>){
 		mOutputPort = port
 	}
-	/* Do not call this method from the outside */
-	public override func flush(string str: String){
+	public override func print(string str: String){
 		mOutputPort.output(data: str)
 	}
 }
